e40131ca8719f2a537de84924da1cd7dedf4cff6
Mykolai Buriak <nikolaybeet@gmail.com>
2023-11-25 13:24:24 +0200

    Terraform loadbalancer delete (#6)
    * add terraform cloud conf
    
    * fix terraform backend
    
    * add github actions workflow for terraform
    
    * change github workflow on policy to every branch
    
    * add paths rule to run terraform workflow
    
    * fix workflow on path policy for terraform
    
    * fix workflow terraform working directory
    
    * fix terraform workflow envs
    
    * fix terraform workflow
    
    * fix terraform workflow
    
    * change backend conf
    
    * fix terraform workflow envs
    
    * terraform fmt, add lock.hcl to gitignore
    
    * fix terraform workflow
    
    * fix terraform workflow
    
    * fix terraform workflow
    
    * add ingress-nginx helm
    
    * add workflow to deploy helm ingress-nginx
    
    * change on policy to run terraform workflow
    
    * fix helm ingress-nginx workflow
    
    * change image for workflow helm ingress-nginx
    
    * fix helm ingress-nginx workflow
    
    * add working directory to jobs in helm ingress-nginx workflow
    
    * add workflow_dispatch to terraform and helm workflows
    
    * fix workflows
    
    * fix workflows
    
    * change helm ingress-nginx workflow to docker run
    
    * fix helm ingress-nginx workflow
    
    * fix helm ingress-nginx workflow
    
    * fix helm ingress-nginx workflow
    
    * fix helm ingress-nginx workflow
    
    * use docker as image to run helm ingress workflow
    
    * change volume using
    
    * change volume using
    
    * change volume using
    
    * change path to helm ingress dir
    
    * change run command
    
    * change kubeconfig env
    
    * add kubeconfig file
    
    * add kubeconfig file
    
    * add kubeconfig file
    
    * add kubeconfig file
    
    * add kubeconfig file
    
    * add kubeconfig file
    
    * add debug info
    
    * add volume mapping
    
    * add volume mapping
    
    * add debug info
    
    * fix helm ingress workflow
    
    * fix helm ingress workflow
    
    * add debug info
    
    * add context
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * fix workflow
    
    * add debug info
    
    * add kube context
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * add debug info
    
    * fix workflow
    
    * fix workflow
    
    * enable hostport
    
    * change target port in loadbalancer
    
    * delete loadbalancer from terraform
    
    * delete loadbalancer from terraform

--- infra/terraform/networking.tf
+++ infra/terraform/networking.tf
@@ -15,25 +15,3 @@ resource "digitalocean_firewall" "masters_degree_main" {
     source_addresses = ["0.0.0.0/0"]
   }
 }
-
-resource "digitalocean_loadbalancer" "masters_degree" {
-  name      = "masters-degree"
-  region    = "fra1"
-  vpc_uuid  = digitalocean_vpc.masters_degree.id
-  size_unit = 1
-
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "tcp"
-
-    target_port     = 30391
-    target_protocol = "tcp"
-  }
-
-  healthcheck {
-    port     = 443
-    protocol = "tcp"
-  }
-
-  droplet_tag = "k8s:worker"
-}
